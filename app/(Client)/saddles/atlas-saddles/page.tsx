57500d5bc18e6c02e006aca1ed9fbaf85320801d
Saidul147 <sidul.islam.146@gmail.com>
2025-09-22 10:46:29 +0600

    athena saddles done

--- app/(Client)/saddles/atlas-saddles/page.tsx
+++ app/(Client)/saddles/atlas-saddles/page.tsx
@@ -1,7 +1,41 @@
+import SAddlesCategoryItems from '@/components/ClientComponents/Saddles/SAddlesCategoryItems'
 import React from 'react'
 
+const saddlesItems = {
+    label: "ATLAS SADDLES",
+    subtitle: "full CUSTOM",
+    items: ["JUMPING", "DRESSAGE", "CROSS-COUNTRY", "KIDS"],
+    btns: [
+        { title: "Connect with a saddle expert", href: "" },
+    ],
+    images: [
+        {
+            id: 1,
+            name: "JUMPING SADDLES",
+            image: "/images/saddles/atlas-jumping.png",
+        },
+        {
+            id: 2,
+            name: "CROSS COUNTRY SADDLES",
+            image: "/images/saddles/atlas-cross.png",
+        },
+        {
+            id: 3,
+            name: "DRESSAGE SADDLES",
+            image: "/images/saddles/atlas-dressage.png",
+        },
+        {
+            id: 4,
+            name: "KIDS SADDLES",
+            image: "/images/saddles/atlas-kids.png",
+        },
+    ]
+}
+
 export default function AtlasSaddlesPage() {
-  return (
-    <div>AtlasSaddlesPage</div>
-  )
+    return (
+        <div className='maxContainer lg:py-[80px] py-[60px]'>
+            <SAddlesCategoryItems  saddlesItems = {saddlesItems} />
+        </div>
+    )
 }
