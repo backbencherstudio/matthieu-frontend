a737a9459b651200f6fa4b9a0720b7bc35415f5a
Saidul147 <sidul.islam.146@gmail.com>
2025-09-23 14:44:46 +0600

    bridles page addded

--- app/(Client)/tack/bridles/page.tsx
+++ app/(Client)/tack/bridles/page.tsx
@@ -1,7 +1,38 @@
+import BreadCrumb from '@/components/ClientComponents/reuseable/BreadCrumb';
+import FilteredCategory from '@/components/ClientComponents/Tacks/FilteredCategory';
 import React from 'react'
 
 export default function BridlesPage() {
+
+  const breadLink = [
+    {
+      label: "TACK",
+      href: "/TACK",
+    },
+    {
+      label: "BRIDLES",
+      href: "/tack/bridles",
+    },
+  ];
+
   return (
-    <div>BridlesPage</div>
+    <div>
+      <BreadCrumb breadLink={breadLink} />
+      <div className='maxContainer my-12'>
+        <FilteredCategory categoryData={categoryItems} title="Bridles" />
+      </div>
+    </div>
   )
 }
+
+const categoryItems = [
+  { id: 1, name: "Anatomical Double Noseband Bridle", category: "Bridles", image: "/home/bridles.png", price: "$370.00", color: "Black", size: "Full" },
+  { id: 2, name: "Anatomical Double Noseband Bridle", category: "Bridles", image: "/home/bridles.png", price: "$370.00", color: "Brown", size: "Cob" },
+  { id: 3, name: "Anatomical Double Noseband Bridle", category: "Bridles", image: "/home/bridles.png", price: "$370.00", color: "Black", size: "Pony" },
+  { id: 4, name: "Anatomical Double Noseband Bridle", category: "Bridles", image: "/home/bridles.png", price: "$370.00", color: "Brown", size: "Full" },
+  { id: 5, name: "Anatomical Double Noseband Bridle", category: "Bridles", image: "/home/bridles.png", price: "$370.00", color: "Black", size: "Cob" },
+  { id: 6, name: "Anatomical Double Noseband Bridle", category: "Bridles", image: "/home/bridles.png", price: "$370.00", color: "Brown", size: "Pony" },
+  { id: 7, name: "Anatomical Double Noseband Bridle", category: "Bridles", image: "/home/bridles.png", price: "$370.00", color: "Black", size: "Full" },
+  { id: 8, name: "Anatomical Double Noseband Bridle", category: "Bridles", image: "/home/bridles.png", price: "$370.00", color: "Brown", size: "Cob" },
+
+];
--- app/(Client)/tack/reins/page.tsx
+++ app/(Client)/tack/reins/page.tsx
@@ -2,8 +2,7 @@ import BreadCrumb from '@/components/ClientComponents/reuseable/BreadCrumb';
 import FilteredCategory from '@/components/ClientComponents/Tacks/FilteredCategory';
 import React from 'react'
 
-export default function
-  () {
+export default function ReinsPage() {
 
   const breadLink = [
     {
@@ -20,20 +19,20 @@ export default function
     <div>
       <BreadCrumb breadLink={breadLink} />
       <div className='maxContainer my-12'>
-        <FilteredCategory categoryData = {reinsItems} title = "Reins" />
+        <FilteredCategory categoryData={categoryItems} title="Reins" />
       </div>
     </div>
   )
 }
 
-const reinsItems = [
-    { id: 1, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack1.png", price: "$370.00", color: "Black", size: "Standard" },
-    { id: 2, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack2.png", price: "$370.00", color: "Brown", size: "Standard" },
-    { id: 3, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack3.png", price: "$370.00", color: "Black", size: "Long" },
-    { id: 4, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack4.png", price: "$370.00", color: "Brown", size: "Long" },
-    { id: 5, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack1.png", price: "$370.00", color: "Black", size: "Standard" },
-    { id: 6, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack2.png", price: "$370.00", color: "Brown", size: "Standard" },
-    { id: 7, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack3.png", price: "$370.00", color: "Black", size: "Long" },
-    { id: 8, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack4.png", price: "$370.00", color: "Brown", size: "Long" },
+const categoryItems = [
+  { id: 1, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack1.png", price: "$370.00", color: "Black", size: "Standard" },
+  { id: 2, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack2.png", price: "$370.00", color: "Brown", size: "Standard" },
+  { id: 3, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack3.png", price: "$370.00", color: "Black", size: "Long" },
+  { id: 4, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack4.png", price: "$370.00", color: "Brown", size: "Long" },
+  { id: 5, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack1.png", price: "$370.00", color: "Black", size: "Standard" },
+  { id: 6, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack2.png", price: "$370.00", color: "Brown", size: "Standard" },
+  { id: 7, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack3.png", price: "$370.00", color: "Black", size: "Long" },
+  { id: 8, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack4.png", price: "$370.00", color: "Brown", size: "Long" },
 
 ];
