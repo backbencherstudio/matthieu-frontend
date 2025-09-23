ac4152eb4215f30cdee9e8fba65d94116907e604
Saidul147 <sidul.islam.146@gmail.com>
2025-09-23 14:40:55 +0600

    reins page done

--- app/(Client)/tack/reins/page.tsx
+++ app/(Client)/tack/reins/page.tsx
@@ -1,5 +1,5 @@
 import BreadCrumb from '@/components/ClientComponents/reuseable/BreadCrumb';
-import ReinsCategory from '@/components/ClientComponents/Tacks/SubCategory/Reints'
+import FilteredCategory from '@/components/ClientComponents/Tacks/FilteredCategory';
 import React from 'react'
 
 export default function
@@ -20,8 +20,20 @@ export default function
     <div>
       <BreadCrumb breadLink={breadLink} />
       <div className='maxContainer my-12'>
-        <ReinsCategory />
+        <FilteredCategory categoryData = {reinsItems} title = "Reins" />
       </div>
     </div>
   )
 }
+
+const reinsItems = [
+    { id: 1, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack1.png", price: "$370.00", color: "Black", size: "Standard" },
+    { id: 2, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack2.png", price: "$370.00", color: "Brown", size: "Standard" },
+    { id: 3, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack3.png", price: "$370.00", color: "Black", size: "Long" },
+    { id: 4, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack4.png", price: "$370.00", color: "Brown", size: "Long" },
+    { id: 5, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack1.png", price: "$370.00", color: "Black", size: "Standard" },
+    { id: 6, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack2.png", price: "$370.00", color: "Brown", size: "Standard" },
+    { id: 7, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack3.png", price: "$370.00", color: "Black", size: "Long" },
+    { id: 8, name: "Reins with handles", category: "Reins", image: "/images/tacks/tack4.png", price: "$370.00", color: "Brown", size: "Long" },
+
+];
