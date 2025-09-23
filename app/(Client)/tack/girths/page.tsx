e54fe983b45af788fd6c49a9c5c0872553ed9e6e
Saidul147 <sidul.islam.146@gmail.com>
2025-09-23 15:07:00 +0600

    halters and ropes page added

--- app/(Client)/tack/girths/page.tsx
+++ app/(Client)/tack/girths/page.tsx
@@ -1,7 +1,37 @@
+import BreadCrumb from '@/components/ClientComponents/reuseable/BreadCrumb';
+import FilteredCategory from '@/components/ClientComponents/Tacks/FilteredCategory';
 import React from 'react'
 
 export default function GirthsPage() {
+
+  const breadLink = [
+    {
+      label: "TACK",
+      href: "/TACK",
+    },
+    {
+      label: "GIRTHS",
+      href: "/tack/girths",
+    },
+  ];
+
   return (
-    <div>GirthsPage</div>
+    <div>
+      <BreadCrumb breadLink={breadLink} />
+      <div className='maxContainer my-12'>
+        <FilteredCategory categoryData={categoryItems} title="girths" />
+      </div>
+    </div>
   )
 }
+
+const categoryItems = [
+  { id: 1, name: "Short dressage girth", category: "Girths", image: "/home/grith.png", price: "$370.00", color: "Black", size: "24inch" },
+  { id: 2, name: "Short dressage girth", category: "Girths", image: "/home/grith.png", price: "$370.00", color: "Brown", size: "26inch" },
+  { id: 3, name: "Short dressage girth", category: "Girths", image: "/home/grith.png", price: "$370.00", color: "Black", size: "28inch" },
+  { id: 4, name: "Short dressage girth", category: "Girths", image: "/home/grith.png", price: "$370.00", color: "Brown", size: "24inch" },
+  { id: 5, name: "Short dressage girth", category: "Girths", image: "/home/grith.png", price: "$370.00", color: "Black", size: "26inch" },
+  { id: 6, name: "Short dressage girth", category: "Girths", image: "/home/grith.png", price: "$370.00", color: "Brown", size: "28inch" },
+  { id: 7, name: "Short dressage girth", category: "Girths", image: "/home/grith.png", price: "$370.00", color: "Black", size: "24inch" },
+  { id: 8, name: "Short dressage girth", category: "Girths", image: "/home/grith.png", price: "$370.00", color: "Brown", size: "26inch" },
+];
--- app/(Client)/tack/halters-and-ropes/page.tsx
+++ app/(Client)/tack/halters-and-ropes/page.tsx
@@ -1,7 +1,30 @@
+import BreadCrumb from '@/components/ClientComponents/reuseable/BreadCrumb';
+import FilteredCategory from '@/components/ClientComponents/Tacks/FilteredCategory';
 import React from 'react'
 
-export default function HalterRopesPage() {
+export default function HalterRopePage() {
+
+  const breadLink = [
+    {
+      label: "TACK",
+      href: "/TACK",
+    },
+    {
+      label: "HALTERS & ROPES",
+      href: "/tack/halters-and-ropes",
+    },
+  ];
+
   return (
-    <div>HalterRopesPage</div>
+    <div>
+      <BreadCrumb breadLink={breadLink} />
+      <div className='maxContainer my-12'>
+        <FilteredCategory categoryData={categoryItems} title="HALTERS & ROPES" />
+      </div>
+    </div>
   )
 }
+
+const categoryItems = [
+    { id: 1, name: "Halter lead rope", category: "HALTERS & ROPES", image: "/images/tacks/halter.jpg", price: "$370.00", color: "Black", size: "Standard" },
+];
