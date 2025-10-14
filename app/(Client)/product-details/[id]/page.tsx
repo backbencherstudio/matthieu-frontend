9974765c0210a7d387ac3c90a16f7e61cf8d4f8a
Saidul147 <sidul.islam.146@gmail.com>
2025-10-14 11:49:14 +0600

    responsive added on product details

--- app/(Client)/product-details/[id]/page.tsx
+++ app/(Client)/product-details/[id]/page.tsx
@@ -21,8 +21,8 @@ export default function ProductDetailsPage() {
     const [quantity, setQuantity] = useState(1);
 
     const colors = [
-        { name: 'BLACK', code: '#070707' },
-        { name: 'BROWN', code: '#6E412A' }
+        { name: 'BLACK', code: 'bg-[#070707]' },
+        { name: 'BROWN', code: 'bg-[#6E412A]' }
     ];
 
     const sizes = [
@@ -49,18 +49,20 @@ export default function ProductDetailsPage() {
         <div>
             <BreadCrumb breadLink={breadLink} cart={true} />
             <div className=' lg:py-12 md:py-10 py-8 maxContainer'>
-                <div className='flex gap-6'>
+                <div className='flex xl:flex-row flex-col gap-6'>
                     {/* image div */}
-                    <div className='flex flex-col gap-6'>
-                        {productImages?.map((img, index) => (
-                            <img key={index} src={img?.imgLink} alt="" />
-                        ))}
+                    <div className='xl:block hidden'>
+                        <div className='xl:flex xl:flex-col grid grid-cols-4 gap-4 '>
+                            {productImages?.map((img, index) => (
+                                <img key={index} src={img?.imgLink} alt="" className='object-contain w-full h-full ' />
+                            ))}
+                        </div>
                     </div>
                     <div className='w-full'>
                         <div className="  bg-white ">
-                            <div className="grid grid-cols-2 gap-8">
+                            <div className="grid md:grid-cols-2 xl:gap-8 lg:gap-6 gap-4">
                                 {/* Image Section */}
-                                <div className='border'>
+                                <div className='flex flex-col gap-3'>
                                     <div className="flex items-center justify-center  ">
                                         <img
                                             src="/images/products-details/Rectangle 6602.png"
@@ -68,37 +70,44 @@ export default function ProductDetailsPage() {
                                             className="w-full h-full object-cover "
                                         />
                                     </div>
+                                    <div className='block xl:hidden'>
+                                        <div className='xl:flex xl:flex-col grid grid-cols-4 gap-4 '>
+                                            {productImages?.map((img, index) => (
+                                                <img key={index} src={img?.imgLink} alt="" className='object-contain w-full h-full ' />
+                                            ))}
+                                        </div>
+                                    </div>
                                 </div>
 
                                 {/* Product Details Section */}
-                                <div className="flex flex-col gap-8">
+                                <div className="flex flex-col xl:gap-8 lg:gap-6 gap-4">
 
                                     {/* Header */}
-                                    <div className="flex flex-col gap-4">
+                                    <div className="flex flex-col xl:gap-4 gap-3">
                                         <div className='flex justify-between items-center'>
-                                            <h1 className="lg:text-[32px] md:text-[28px] text-2xl font-extrabold leading-[100%] tracking-[0.64px] text-[#1F274B] ">DOUBLE NOSEBAND BRIDLE</h1>
+                                            <h1 className="xl:text-[32px] text-2xl font-extrabold leading-[100%] tracking-[0.64px] text-[#1F274B] ">DOUBLE NOSEBAND BRIDLE</h1>
                                             <button className="">
-                                                <LoveIcon className="" />
+                                                <LoveIcon className="xl:size-12 size-8" />
                                             </button>
                                         </div>
-                                        <p className="lg:text-2xl md:text-xl text-lg leading-[100%] trtacking-[.48px] text-[#4C526F]">SKU: BR7D2NS</p>
+                                        <p className="xl:text-2xl md:text-xl text-lg leading-[100%] trtacking-[.48px] text-[#4C526F]">SKU: BR7D2NS</p>
                                     </div>
 
-                                    <div className='h-[1px] bg-[#DFE1E7] w-full block'></div>
-                                    <p className="lg:text-[32px] md:text-[28px] text-2xl tracking-[.64px] text-[#1F274B] leading-[100%]">US $580.00</p>
+                                    <div className='h-[1px] bg-[#DFE1E7] w-full '></div>
+                                    <p className="xl:text-[32px]  text-2xl tracking-[.64px] text-[#1F274B] leading-[100%]">US $580.00</p>
                                     {/* Color Selection */}
-                                    <div className="flex flex-col gap-4">
-                                        <p className="text-[#262626] lg:text-[20px] md:text-lg text-base tracking-[.40px] leading-[100%]">Color:</p>
+                                    <div className="flex flex-col xl:gap-4 gap-3">
+                                        <p className="text-[#262626] xl:text-[20px]  text-base tracking-[.40px] leading-[100%]">Color:</p>
                                         <div className='flex  gap-4'>
 
-                                            {colors.map((color,index) => (
+                                            {colors.map((color, index) => (
                                                 <div key={index} className='flex flex-col justify-between items-center gap-2'>
-                                                    <div className={`w-8 h-8 bg-[${color?.code}]`}>
+                                                    <div className={`xl:w-8 xl:h-8 w-6 h-6 ${color?.code}`}>
                                                     </div>
                                                     <button
                                                         key={color.name}
                                                         onClick={() => setSelectedColor(color.name)}
-                                                        className={`text-[#4C526F] text-base leading-[100%] tracking-[.32px] transition-all font-light`}
+                                                        className={`text-[#4C526F] xl:text-base text-sm leading-[100%] tracking-[.32px] transition-all font-light`}
                                                     >
                                                         {color.name}
                                                     </button>
@@ -109,17 +118,17 @@ export default function ProductDetailsPage() {
                                     </div>
 
                                     {/* Size Selection */}
-                                    <div className="flex flex-col gap-4">
+                                    <div className="flex flex-col xl:gap-4 gap-3">
                                         <div className="flex justify-between items-center">
-                                            <p className="text-[#262626] leading-[100%] tracking-[.4px] lg:text-xl md:text-lg text-base ">Size:</p>
+                                            <p className="text-[#262626] leading-[100%] tracking-[.4px] xl:text-xl md:text-lg text-base ">Size:</p>
                                             <button className="underline text-base leading-[100%] tracking-[.32px] text-[#4C526F] cursor-pointer underline-offset-2 hover:no-underline">See Size Guide</button>
                                         </div>
-                                        <div className="flex gap-3">
+                                        <div className="grid md:grid-cols-4 grid-cols-2 md:gap-3 gap-2">
                                             {sizes.map((size) => (
                                                 <button
                                                     key={size.id}
                                                     onClick={() => setSelectedSize(size.id.toUpperCase())}
-                                                    className={`py-3 w-full text-base  transition-all bg-[#4C526F] hover:bg-[#4C526F] leading-[100%] cursor-pointer ${selectedSize === size.id.toUpperCase()
+                                                    className={`md:py-3 py-2 w-full  md:text-base text-sm  transition-all bg-[#4C526F] hover:bg-[#4C526F] leading-[100%] cursor-pointer ${selectedSize === size.id.toUpperCase()
                                                         ? 'bg-[#4C526F] text-white font-extrabold'
                                                         : 'bg-[#696E86] text-white '
                                                         }`}
@@ -132,11 +141,11 @@ export default function ProductDetailsPage() {
                                     </div>
 
                                     {/* Benefits Section */}
-                                    <div className="flex flex-col gap-4">
-                                        <p className="lg:text-xl md:text-lg text-base text-[#262626] leading-[100%] tracking-[.4px]">Benefits:</p>
-                                        <ul className="space-y-3">
+                                    <div className="flex flex-col xl:gap-4 gap-3">
+                                        <p className="lg:text-xl  text-base text-[#262626] leading-[100%] tracking-[.4px]">Benefits:</p>
+                                        <ul className="xl:space-y-3 space-y-2">
                                             {benefits.map((benefit, index) => (
-                                                <li key={index} className="flex gap-2.5 md:text-base text-sm tracking-[.08px] leading-[120%] text-[#4C526F]">
+                                                <li key={index} className="flex gap-2.5 xl:text-base text-sm tracking-[.08px] leading-[120%] text-[#4C526F]">
                                                     <span className="  font-bold ">•</span>
                                                     <span >{benefit}</span>
                                                 </li>
@@ -144,23 +153,23 @@ export default function ProductDetailsPage() {
                                         </ul>
                                     </div>
 
-                                    <button className="text-[#262626] md:text-lg text-start text-base underline underline-offset-2">
+                                    <button className="text-[#262626] xl:text-lg text-start text-base underline underline-offset-2">
                                         More details
                                     </button>
                                     {/* Buttons Section */}
-                                    <div className="space-y-4">
+                                    <div className="xl:space-y-4 space-y-3">
                                         <div className='flex gap-3'>
-                                            <button className="w-full bg-[#1F274B] text-white flex items-center justify-center cursor-pointer">
-                                                <span className='flex items-center gap-2 '>BUY WITH <AppleIcon/></span>
-                                                
+                                            <button className="w-full bg-[#1F274B] hover:bg-[#1F274B]/90 text-white flex items-center justify-center cursor-pointer">
+                                                <span className='flex items-center gap-2 '>BUY WITH <AppleIcon /></span>
+
                                             </button>
 
-                                            <button className="w-full bg-white hover:bg-gray-50 text-gray-900 font-bold py-3 px-4 border-1 border-[#1F274B] transition">
+                                            <button className="w-full cursor-pointer bg-white hover:bg-gray-50 text-gray-900 font-bold py-3 px-4 border-1 border-[#1F274B] transition">
                                                 ADD TO CART
                                             </button>
                                         </div>
 
-                                        <button className="text-[#262626] md:text-lg text-start text-base underline underline-offset-2">
+                                        <button className="text-[#262626] xl:text-lg text-start text-base underline underline-offset-2">
                                             More payment options
                                         </button>
                                     </div>
