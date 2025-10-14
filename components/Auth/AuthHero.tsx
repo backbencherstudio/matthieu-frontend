157d91993a54c95f4927a7ccba173ff8f42976e9
Parvez <parvezbbstudio@gmail.com>
2025-10-14 14:31:48 +0600

    [DONE] Sign up design issue solved

--- components/Auth/AuthHero.tsx
+++ components/Auth/AuthHero.tsx
@@ -9,13 +9,13 @@ export function AuthHero({ heroImage, title }: AuthHeroProps) {
   return (
     <div className="maxContainer">
       {/* Hero Image Section */}
-      <div className="relative w-full xl:w-[1480px] xl:h-[380px] lg:w-[1480px] lg:h-[380px]">
+      <div className="relative w-full  xl:py-[165px] lg:py-[100px] md:py-[80px] py-[60px]">
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{ backgroundImage: `url(${heroImage})` }}
         />
-        <div className="relative h-full flex items-center justify-center p-8">
-          <h1 className="text-[40px] font-extrabold text-white text-center font-avenir">
+        <div className="relative h-full flex items-center justify-center ">
+          <h1 className="text-[40px] font-extrabold text-white text-center leading-[124%]">
             {title}
           </h1>
         </div>
--- components/Auth/sign-up-form.tsx
+++ components/Auth/sign-up-form.tsx
@@ -43,17 +43,15 @@ export function SignUpForm() {
     <div className="maxContainer my-12">
       <div className="w-full xl:w-[464px] lg:w-[464px] mx-auto border p-[32px]">
         <CardHeader className="space-y-1 text-center">
-          <CardTitle className="text-[32px] font-extrabold font-avenir">
-            Sign Up
-          </CardTitle>
+          <CardTitle className="text-[32px] font-extrabold ">Sign Up</CardTitle>
         </CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-            <div className="flex items-center gap-[19px]">
-              <div>
+            <div className="flex items-center gap-[19px] self-stretch">
+              <div className="flex flex-col gap-2">
                 <Label
                   htmlFor="firstName"
-                  className="text-[16px] font-normal font-avenir text-[#4A4C56]"
+                  className="text-[16px] font-normal text-[#4A4C56]"
                 >
                   Fist Name
                 </Label>
@@ -61,7 +59,7 @@ export function SignUpForm() {
                   id="firstName"
                   type="text"
                   placeholder="Yasir Abed"
-                  className="bg-[#DFE1E7] rounded-none px-4 py-3 focus-visible:ring-0"
+                  className="bg-[#E9E9ED80] rounded-none !h-auto leading-[150%] lg:px-4 px-3 lg:py-3 py-2  focus-visible:ring-0 focus-visible:border-[#1F274B]"
                   {...register("firstName", {
                     required: "First Name is required",
                   })}
@@ -72,10 +70,10 @@ export function SignUpForm() {
                   </p>
                 )}
               </div>
-              <div>
+              <div className="flex flex-col gap-2">
                 <Label
                   htmlFor="lastName"
-                  className="text-[16px] font-normal font-avenir text-[#4A4C56]"
+                  className="text-[16px] font-normal text-[#4A4C56]"
                 >
                   Last Name
                 </Label>
@@ -83,7 +81,7 @@ export function SignUpForm() {
                   id="lastName"
                   type="text"
                   placeholder="Rabbu"
-                  className="bg-[#DFE1E7] rounded-none px-4 py-3 focus-visible:ring-0"
+                  className="bg-[#E9E9ED80] rounded-none !h-auto leading-[150%] lg:px-4 px-3 lg:py-3 py-2 focus-visible:ring-0 focus-visible:border-[#1F274B]"
                   {...register("lastName", {
                     required: "Last Name is required",
                   })}
@@ -98,7 +96,7 @@ export function SignUpForm() {
             <div className="space-y-2">
               <Label
                 htmlFor="email"
-                className="text-[16px] font-normal font-avenir text-[#4A4C56]"
+                className="text-[16px] font-normal  text-[#4A4C56]"
               >
                 Email
               </Label>
@@ -106,7 +104,7 @@ export function SignUpForm() {
                 id="email"
                 type="email"
                 placeholder="arionadmin@gmail.com"
-                className="bg-[#DFE1E7] rounded-none px-4 py-3 focus-visible:ring-0"
+                className="bg-[#E9E9ED80] rounded-none !h-auto leading-[150%] lg:px-4 px-3 lg:py-3 py-2 focus-visible:ring-0 focus-visible:border-[#1F274B]"
                 {...register("email", {
                   required: "Email is required",
                   pattern: {
@@ -125,7 +123,7 @@ export function SignUpForm() {
             <div className="space-y-2">
               <Label
                 htmlFor="password"
-                className="text-[16px] font-normal font-avenir text-[#4A4C56]"
+                className="text-[16px] font-normal  text-[#4A4C56]"
               >
                 Password
               </Label>
@@ -133,7 +131,7 @@ export function SignUpForm() {
                 id="password"
                 type="password"
                 placeholder="••••••••••"
-                className="bg-[#DFE1E7] rounded-none px-4 py-3 focus-visible:ring-0"
+                className="bg-[#E9E9ED80] rounded-none !h-auto leading-[150%] lg:px-4 px-3 lg:py-3 py-2 focus-visible:ring-0 focus-visible:border-[#1F274B]"
                 {...register("password", {
                   required: "Password is required",
                   minLength: {
@@ -151,7 +149,7 @@ export function SignUpForm() {
             <div className="space-y-2">
               <Label
                 htmlFor="confirmPassword"
-                className="text-[16px] font-normal font-avenir text-[#4A4C56]"
+                className="text-[16px] font-normal text-[#4A4C56]"
               >
                 Confirm Password
               </Label>
@@ -159,7 +157,7 @@ export function SignUpForm() {
                 id="confirmPassword"
                 type="password"
                 placeholder="••••••••••"
-                className="bg-[#DFE1E7] rounded-none px-4 py-3 focus-visible:ring-0"
+                className="bg-[#E9E9ED80] rounded-none !h-auto leading-[150%] lg:px-4 px-3 lg:py-3 py-2 focus-visible:ring-0 focus-visible:border-[#1F274B]"
                 {...register("confirmPassword", {
                   required: "Confirm Password is required",
                   minLength: {
@@ -175,29 +173,29 @@ export function SignUpForm() {
               )}
             </div>
 
-            <div className="space-y-4">
+            <div>
               <div className="flex items-center space-x-2">
                 <Checkbox
                   id="subscribe"
                   {...register("subscribe")}
-                  className="w-[14px] h-[14px] aspect-square bg-white"
+                  className="!w-[14px] !h-[14px] aspect-square rounded-[3px] bg-[#FFF]"
                 />
                 <Label
                   htmlFor="subscribe"
-                  className="text-sm font-normal text-[#4A4C56] font-avenir"
+                  className="text-sm font-normal text-[#4A4C56] leading-[100%]"
                 >
                   Subscribe to our newsletter
                 </Label>
               </div>
-              <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-2 mt-[7px]">
                 <Checkbox
                   id="conditions"
                   {...register("conditions")}
-                  className="w-[14px] h-[14px] aspect-square bg-white"
+                  className="!w-[14px] !h-[14px] aspect-square rounded-[3px] bg-[#FFF]"
                 />
                 <Label
                   htmlFor="conditions"
-                  className="text-sm font-normal text-[#4A4C56] font-avenir"
+                  className="text-sm font-normal text-[#4A4C56] leading-[100%]"
                 >
                   I agree to the Terms & Conditions
                 </Label>
@@ -206,30 +204,28 @@ export function SignUpForm() {
 
             <Button
               type="submit"
-              className="w-full bg-[#1F274B] cursor-pointer hover:bg-[#1F274B]/90 rounded-none px-4 py-3 text-[#FFF] font-avenir"
+              className="w-full bg-[#1F274B] cursor-pointer hover:bg-[#1F274B]/90 rounded-none !h-auto leading-[100%] lg:px-8 px-7 lg:py-4 py-3 text-[#FFF] self-stretch text-[16px] !my-[40px]"
             >
               Create Account
             </Button>
 
-            <div className="text-center text-[18px]">
-              <span className="text-[#4A4C56] font-avenir">
-                Already have an account?{" "}
-              </span>
+            <div className="text-center text-[18px] leading-[100%]">
+              <span className="text-[#4A4C56] ">Already have an account? </span>
               <Link
                 href="/signin"
-                className="font-avenir font-extrabold text-[#2A3988] hover:underline"
+                className=" font-extrabold text-[#2A3988] hover:underline"
               >
                 Sign In
               </Link>
             </div>
 
             <div className="flex justify-center text-xs">
-              <span className="text-[#717182] text-[16px] font-avenir">or</span>
+              <span className="text-[#717182] text-[16px] ">or</span>
             </div>
 
             <Button
               type="button"
-              className="w-full bg-[#FFF] rounded-none px-4 py-3 text-[#4A4C56] border broder-[#DFE1E7] hover:bg-[#FFF]/50 cursor-pointer"
+              className="w-full bg-[#FFF] rounded-none !h-auto leading-[100%] lg:px-8 px-7 lg:py-4 py-3 text-[#4A4C56] border broder-[#DFE1E7] hover:bg-[#FFF]/50 cursor-pointer"
               onClick={() => console.log("Continue as guest")}
             >
               Continue as Guest
