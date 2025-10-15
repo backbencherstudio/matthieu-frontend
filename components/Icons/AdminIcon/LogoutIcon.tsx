850a715420cdcea3c1db952e5019257972cd77b6
Parvez <parvezz13913@gmail.com>
2025-10-15 15:32:19 +0600

    [DONE] My Order page design

--- components/Icons/AdminIcon/LogoutIcon.tsx
+++ components/Icons/AdminIcon/LogoutIcon.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function LogoutIcon({ className }: { className?: string }) {
   return (
     <svg
@@ -13,16 +11,16 @@ export default function LogoutIcon({ className }: { className?: string }) {
       <path
         d="M6.33333 2.5C5.55836 2.5 5.17087 2.5 4.85295 2.58518C3.99022 2.81635 3.31636 3.49022 3.08518 4.35295C3 4.67087 3 5.05836 3 5.83333V14.1667C3 14.9417 3 15.3292 3.08518 15.6471C3.31635 16.5098 3.99022 17.1837 4.85295 17.4148C5.17087 17.5 5.55836 17.5 6.33333 17.5"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M14.2475 13.75C14.2475 13.75 17.9974 10.9882 17.9974 10C17.9974 9.01175 14.2474 6.25 14.2474 6.25M17.164 10H7.16406"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- components/MyAccount/sidebar.tsx
+++ components/MyAccount/sidebar.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import AddressBookIcon from "@/components/Icons/MyAccoountIcon/AddressBookIcon";
+import DashboardLayoutIcon from "@/components/Icons/MyAccoountIcon/DashboardLayoutIcon";
+import HeartIcon from "@/components/Icons/MyAccoountIcon/HeartIcon";
+import SideBarShoppingIcon from "@/components/Icons/MyAccoountIcon/SideBarShoppingIcon";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import AddressBookIcon from "@/public/icon/AddressBookIcon";
-import DashboardLayoutIcon from "@/public/icon/DashboardLayoutIcon";
-import HeartIcon from "@/public/icon/HeartIcon";
-import SideBarShoppingIcon from "@/public/icon/SideBarShoppingIcon";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import LogoutIcon from "../Icons/AdminIcon/LogoutIcon";
 import SettingIcon from "../Icons/AdminIcon/SettingIcon";
+import CameraIcon from "../Icons/MyAccoountIcon/CameraIcon";
 
 const menuItems = [
   {
@@ -18,7 +20,7 @@ const menuItems = [
     label: "DASHBOARD",
     href: "/my-account/dashboard",
   },
-  { icon: SideBarShoppingIcon, label: "ORDERS", href: "/orders" },
+  { icon: SideBarShoppingIcon, label: "ORDERS", href: "/my-account/orders" },
   { icon: AddressBookIcon, label: "MY ADDRESS", href: "/address" },
   { icon: HeartIcon, label: "SAVED", href: "/saved" },
   { icon: SettingIcon, label: "SETTINGS", href: "/settings" },
@@ -26,24 +28,57 @@ const menuItems = [
 
 export function Sidebar() {
   const pathname = usePathname();
+  const [preview, setPreview] = useState("/myAccount/profile.jpg");
+
+  // handle file upload
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const fileURL = URL.createObjectURL(file);
+    setPreview(fileURL);
+
+    console.log("Selected file:", file);
+  };
 
   return (
     <aside className="w-2xs h-[740px] border border-[#DFE1E7] bg-[#F6F8FA] p-3">
       <div className="flex flex-col h-full">
         {/* Profile Section */}
-        <div className="flex flex-col items-center p-4">
-          <Avatar className="h-20 w-20 mb-6">
-            <AvatarImage src="/myAccount/profile.jpg" alt="Yasir Abed Rabbu" />
-            <AvatarFallback>YA</AvatarFallback>
-          </Avatar>
+        <div className="flex flex-col p-4 mb-12">
+          <div className="relative w-20 h-20">
+            <input
+              id="avatarUploader"
+              type="file"
+              accept="image/*"
+              onChange={handleFileUpload}
+              className="hidden"
+            />
 
-          <h2 className="text-[20px] font-extrabold uppercase leading-[100%] text-[#1F274B]">
-            YASIR ABED RABBU
-          </h2>
+            <label htmlFor="avatarUploader" className="cursor-pointer block">
+              <Avatar className="w-20 h-20">
+                <AvatarImage src={preview} alt="Profile image" />
+                <AvatarFallback>YA</AvatarFallback>
+              </Avatar>
+            </label>
 
-          <p className="text-[16px] text-[#4C526F] leading-[100%] normal-case mt-2">
-            arionadmin@gmail.com
-          </p>
+            <label
+              htmlFor="avatarUploader"
+              className="absolute top-0 right-0 bg-[#1F274B] rounded-full p-1 border border-white cursor-pointer"
+            >
+              <CameraIcon className="w-4 h-4 text-white" />
+            </label>
+          </div>
+
+          <div className="items-center mt-5">
+            <h2 className="text-[20px] font-extrabold uppercase leading-[100%] text-[#1F274B]">
+              YASIR ABED RABBU
+            </h2>
+
+            <p className="text-[16px] text-[#4C526F] leading-[100%] normal-case mt-2">
+              arionadmin@gmail.com
+            </p>
+          </div>
         </div>
 
         {/* Navigation Menu */}
@@ -56,7 +91,7 @@ export function Sidebar() {
               <Link key={item.href} href={item.href}>
                 <Button
                   className={cn(
-                    "w-full justify-start bg-[#F6F8FA] text-[#4A4C56] hover:bg-[#F6F8FA]/90 text-[16px] font-extrabold uppercase leading-[100%] cursor-pointer rounded-none py-3.5 my-1.5",
+                    "w-full justify-start bg-[#F6F8FA] text-[#4A4C56] hover:bg-[#F6F8FA]/90 text-[16px] font-extrabold uppercase leading-[100%] cursor-pointer rounded-none py-3.5 px-4 my-1.5 !h-auto",
                     isActive &&
                       "w-full justify-start bg-[#1F274B] text-[#FFF] hover:bg-[#1F274B]/90 text-[16px] font-extrabold uppercase leading-[100%] cursor-pointer my-1.5"
                   )}
