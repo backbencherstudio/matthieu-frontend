4a3c9ed0cbedf18cf8d3e2bf9fdacf19e58f5fa7
Saidul147 <sidul.islam.146@gmail.com>
2025-10-19 14:53:27 +0600

    carts responsive done

--- components/ClientComponents/ProductDetailsClient/CartsProducts.tsx
+++ components/ClientComponents/ProductDetailsClient/CartsProducts.tsx
@@ -23,7 +23,7 @@ export default function CartsProducts() {
     return (
         <Drawer direction="right" >
             <DrawerTrigger className="cursor-pointer"> <CartsIcon /></DrawerTrigger>
-            <DrawerContent className="p-8  lg:min-w-[600px] ">
+            <DrawerContent className="p-8  lg:min-w-[600px] md:min-w-[500px] min-w-[90%]">
                 <DrawerHeader className="flex flex-row justify-between items-center p-0 ">
                     <DrawerTitle className="text-[#1F274B] lg:text-[32px] md:text-[28px] text-2xl font-bold leading-[100%]  ">Your Cart</DrawerTitle>
                     <DrawerClose className="cursor-pointer">
