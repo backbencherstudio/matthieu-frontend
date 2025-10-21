c2ac55f285e61a29b9677470f3e4cee41d45e040
Parvez <parvezz13913@gmail.com>
2025-10-21 08:50:48 +0600

    [DONE] Customer page design

--- components/AdminComponents/Customer/Customer.tsx
+++ components/AdminComponents/Customer/Customer.tsx
@@ -1,7 +1,48 @@
+import ActiveCustomerIcon from "@/components/Icons/AdminIcon/ActiveCustomerIcon";
+import DollarIcon from "@/components/Icons/AdminIcon/DollarIcon";
 import LetterIcon from "@/components/Icons/AdminIcon/LetterIcon";
 import PlusIcon from "@/components/Icons/AdminIcon/PlusIcon";
+import UserGroupIcon from "@/components/Icons/AdminIcon/UserGroupIcon";
+import UserPlus from "@/components/Icons/AdminIcon/UserPlus";
+import HeaderCard from "../reusable/HeaderCard";
+import CustomerTable from "./CustomerTable";
 
 export default function Customer() {
+  const CustomerStats = [
+    {
+      id: 1,
+      title: "Total Customers",
+      icon: <UserGroupIcon />,
+      number: "9",
+      iconBgColor: "#EDF3FB",
+      iconColor: "#4A87D7",
+    },
+    {
+      id: 2,
+      title: "Active Customers",
+      icon: <ActiveCustomerIcon />,
+      number: "4",
+      iconBgColor: "#E9FAF7",
+      iconColor: "#22CAAD",
+    },
+    {
+      id: 3,
+      title: "New (30 days)",
+      icon: <UserPlus />,
+      number: "6",
+      iconBgColor: "#FFFAE8",
+      iconColor: "#FFCC00",
+    },
+    {
+      id: 4,
+      title: "Total Revenue",
+      icon: <DollarIcon />,
+      number: "$899.92",
+      iconBgColor: "#F4F1FF",
+      iconColor: "#B081FF",
+    },
+  ];
+
   return (
     <div>
       <div className="flex items-center justify-between mb-5">
@@ -19,6 +60,19 @@ export default function Customer() {
           </button>
         </div>
       </div>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-9">
+        {CustomerStats.map((status) => (
+          <HeaderCard
+            key={status?.id}
+            title={status?.title}
+            icon={status.icon}
+            number={status.number}
+            iconBgColor={status?.iconBgColor}
+            iconColor={status.iconColor}
+          />
+        ))}
+      </div>
+      <CustomerTable />
     </div>
   );
 }
