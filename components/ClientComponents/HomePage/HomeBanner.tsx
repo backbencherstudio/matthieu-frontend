f5e2fbe0bfb7813841b01ea024e467b0e0308308
Parvez <parvezz13913@gmail.com>
2025-11-15 10:19:41 +0600

    [DONE] Home page design

--- components/ClientComponents/HomePage/HomeBanner.tsx
+++ components/ClientComponents/HomePage/HomeBanner.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
@@ -16,26 +15,16 @@ export default function HomeBanner() {
 
   return (
     <div className="maxContainer mb-12 ">
-      <Slider {...settings}>
-        <video
-          autoPlay
-          muted
-          loop
-          className="md:h-[500px] w-full object-cover"
-          poster="/video/thumbnail.png"
-        >
-          <source src="/bannerVideo/homeBanner.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <video autoPlay muted loop className="md:h-[500px] w-full object-cover">
-          <source src="/bannerVideo/homeBanner.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <video autoPlay muted loop className="md:h-[500px] w-full object-cover">
-          <source src="/bannerVideo/homeBanner.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </Slider>
+      <video
+        autoPlay
+        muted
+        loop
+        className="md:h-[500px] w-full object-cover"
+        poster="/video/thumbnail.png"
+      >
+        <source src="/bannerVideo/homeBanner.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
