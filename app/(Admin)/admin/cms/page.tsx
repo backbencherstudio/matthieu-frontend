011ed95fc203c9f2363df105d3b93f1e1bb3f789
parvez13913 <parvezz13913@gmail.com>
2025-11-24 16:37:08 +0600

    [DONE] Arion team tab design

--- app/(Admin)/admin/cms/page.tsx
+++ app/(Admin)/admin/cms/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import ArionTeamTab from "@/components/AdminComponents/Cms/ArionTeamTab";
 import BlogPostTable from "@/components/AdminComponents/Cms/BlogPostTable";
 import CmsTabs from "@/components/AdminComponents/Cms/CmsTabs";
 import ContactTab from "@/components/AdminComponents/Cms/ContactTab";
 import { TabContent } from "@/components/AdminComponents/Settings/SettingsTab";
 import BlogIcon from "@/components/Icons/AdminIcon/BlogIcon";
 import ContactIcon from "@/components/Icons/AdminIcon/ContactIcon";
+import UsersIcon from "@/components/Icons/AdminIcon/UsersIcon";
 import { useState } from "react";
 
 export default function CmsPage() {
@@ -15,6 +17,11 @@ export default function CmsPage() {
       value: "blog",
       label: "Blog Posts",
       icon: <BlogIcon />,
+    },
+    {
+      value: "arion-team",
+      label: "Arion Team",
+      icon: <UsersIcon />,
     },
     { value: "contact", label: "Contact Info", icon: <ContactIcon /> },
   ];
@@ -27,6 +34,9 @@ export default function CmsPage() {
       <div className="w-full">
         <TabContent value="blog" activeTab={activeTab}>
           <BlogPostTable />
+        </TabContent>
+        <TabContent value="arion-team" activeTab={activeTab}>
+          <ArionTeamTab />
         </TabContent>
         <TabContent value="contact" activeTab={activeTab}>
           <ContactTab />
