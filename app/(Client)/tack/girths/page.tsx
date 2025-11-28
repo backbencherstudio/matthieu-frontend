909fcb099f828f55bd67a60426e69ff670e7b8e9
Saidul147 <sidul.islam.146@gmail.com>
2025-11-28 11:41:29 +0600

    girt page return null

--- app/(Client)/tack/girths/page.tsx
+++ app/(Client)/tack/girths/page.tsx
@@ -3,6 +3,7 @@ import FilteredCategory from '@/components/ClientComponents/Tacks/FilteredCatego
 import React from 'react'
 
 export default function GirthsPage() {
+return null;
 
   const breadLink = [
     {
